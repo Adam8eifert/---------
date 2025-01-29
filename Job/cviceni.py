9e6e6b177b4350808c4aa3686ec553950e36b2ef
Adam8eifert <seifert.promotion@gmail.com>
2025-01-29 22:35:28 +0100

    Refactor Tic Tac Toe game by removing old implementation and adding a new version with improved structure and functionality.

--- Job/cviceni.py
+++ Job/cviceni.py
@@ -1,8 +1,1 @@
-prvni_cislo = 5
-druhe_cislo = 5
-
-def vynasob(prvni_cislo, druhe_cislo):
-    vysledek = prvni_cislo * druhe_cislo
-    return vysledek
-
-print(vynasob(prvni_cislo, druhe_cislo))
+print(colorama.__version__)
